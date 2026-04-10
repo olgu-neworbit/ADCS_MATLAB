7d4af85133dd0cba851638bf79c23e97e5901014
olgu-neworbit <olgu@neworbit.space>
2026-04-10 11:46:41 +0100

    earth and sun exlcusion working

--- orbit_params.m
+++ orbit_params.m
@@ -6,10 +6,10 @@ earth.R = 6371e3;
 
 semi_major = earth.R + altitude;
 eccentricity = 0;
-inclination = 60;
-RAAN = 45;
+inclination = 170;
+RAAN = 0;
 periapsis_arg = 0;
-true_anomaly = 45;
+true_anomaly = 10;
 
 %time
 year = 2020;
@@ -29,14 +29,14 @@ NEO.inertia = [30.5, 0, 0; 0, 20, 0; 0, 0, 40.2];
 
 %initialise
 q_icrf2b_initial = [0; -0.38; 0.92; 0];
-omega_icrf2b_initial = [10;-10;10];
+omega_icrf2b_initial = [1;-1;0];
 
-Ts = 0.1;
+Ts = 10;
 
 
 
 %% gyro
-gyro.Ts = 0.1;
+gyro.Ts = 0.5;
 gyro.sigma_u = 1e-3;
 gyro.sigma_v = 1e-3;  %%% this is standard deviation be careful
 
@@ -44,8 +44,14 @@ gyro.sigma_v = 1e-3;  %%% this is standard deviation be careful
 %% star tracker
 
 %ref frame https://www.mdpi.com/1424-8220/18/9/3106 % z along the boresign
-star.boresight_b = [0;0;1];  %% z
-star.x = cross(star.boresight_b,[1;0;0])/norm(cross(star.boresight_b,[1;0;0]));
+star.boresight_b = [0;1;0];  %% z
+
+star.boresight_b = star.boresight_b/norm(star.boresight_b);
+
+star.x = cross(star.boresight_b,[1;0.1;0])/norm(cross(star.boresight_b,[1;0.1;0]));
 star.y = cross(star.boresight_b,star.x)/norm(cross(star.boresight_b,star.x));
 
 star.dcm_b2star = [star.x';star.y';star.boresight_b'];
+
+star.sun_avoidance_angle_deg = 20;
+star.earth_avoidance_angle_deg = 10;
